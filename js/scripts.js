834eb52f06b127a8a5bcb62325665a5f51c63610
Marika Allely and Veronica Alley <Veronica.aubry.alley@gmail.com>
2016-01-28 10:15:51 -0800

    add new minor features

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,29 @@
+//game runs and stops when user gets 6 incorrect letters. ideas for added functions:
+  //alert if user enters something that isnt a letter (including blanks bc that breaks it).
+  //alert if they've already guessed that letter.
+  //alert if they enter more than one letter at a time
+  //condition for winning w/ alert
+  //ability to guess the whole word
+
+
+// this was our attempt to incorporate objects. maybe use it if you want the user to be able to play more than one game and track wins??//
+
+
 var Game = {
-  guessedLetters: [] ,
+  guessedLetters: [],
   score: 0,
+  winorlose: [],
 }
 
 
-var words = ["hello","goodbye", "marmalade"]
-var wordSplit = [];
-var puzzleArray = [];
-var score = 0;
-//generates random word from words array and splits the characters up in its own array called wordSplit; then pushed the corresponding amount of underscores to the puzzleArray//
+//global variables//
+
+var words = ["hello","goodbye", "marmalade"] //library of words//
+var wordSplit = [];    //word split into array of letters//
+var puzzleArray = [];  //array of blanks//
+var score = 0;         //user score//
+
+//generates random word from words array and splits the characters up in its own array called wordSplit; then pushes the corresponding amount of underscores to the puzzleArray//
 
 function getRandomWord() {
 var randomWordIndex = Math.floor(Math.random() * (3 - 0)) + 0;
@@ -21,61 +36,63 @@ var randomWordIndex = Math.floor(Math.random() * (3 - 0)) + 0;
 }
 
 
-
-
 //finds user input letter in the wordSplit and returns indexes of all matches. Then replaces matching indexes on puzzleArray with the guessed letter//
 
 var letterQuest = function(wordSplit, searchLetter){
-  for (var i = 0; i <= wordSplit.length; ++i) {
-    if (wordSplit.slice (i, i + searchLetter.length) == searchLetter) {
-      puzzleArray[i] = searchLetter;
+    for (var i = 0; i <= wordSplit.length; ++i) {
+      if (wordSplit.slice (i, i + searchLetter.length) == searchLetter) { //finds the index positions of letters that match the user input//
+        puzzleArray[i] = searchLetter; //replaces the blanks in the puzzle array with the matching index//
+      }
     }
+    if (puzzleArray.indexOf(searchLetter) === -1) {
+      score = score - 1;
+    }
+    var puzzleString = puzzleArray.join(" ");
+    return puzzleString;
   }
-  if (puzzleArray.indexOf(searchLetter) === -1) {
-    score = score - 1;
-  }
-  return puzzleArray;
-  }
-
-// checks score for duration of game and breaks when the losing condition is met.//
-
-// var scoreCounter = function(){
-//     for (){
-//       if( score === -6){
-//         return true;
-//       //   alert("you lose");
-//       //   break;
-//       // }else run letterquest
-//       }
-//
-//       if (puzzleArray.indexOf("_") === -1) {
-//       alert("you win");
-//       break;
-//     }else run letterquest
-//   }
-//
-// var scoreCounter = function() {
-//   while (count > -6)
-//   return letterQuest();
-// }
 
 
 // User Interface Logic
 $(document).ready(function() {
-  getRandomWord();
-  $("span#puzzle-array").text(puzzleArray);
-  $("form#hangman").submit(function(event) {
-    var searchLetter = $("input#letter").val();
-    var searchLetter = searchLetter.toLowerCase();
-    Game.guessedLetters.push(" " + searchLetter);
-    var results = letterQuest(wordSplit, searchLetter);
-    $("span#puzzle-array").text(puzzleArray);
-    $("span#guessed-letters").text(Game.guessedLetters);
-    console.log(results);
-    console.log(wordSplit);
-    console.log(Game.guessedLetters);
-    event.preventDefault();
+  getRandomWord(); //chooses random word on page load//
+  var puzzleString = puzzleArray.join(" ");
+  $("span#puzzle-array").text(puzzleString);
+  // $("span#puzzle-array").text(puzzleString); //prints blanks to page//
+    $("form#hangman").submit(function(event) {
+      var searchLetter = $("input#letter").val(); //uses user input to define the letter to search//
+      var searchLetter = searchLetter.toLowerCase(); //forces input to be lowercase//
+
+      if(searchLetter) {
+        if (searchLetter.length > 1) {
+          alert("Input invalid!  One at a time, punk.");
+        } else if ((searchLetter === " ") || (isNaN(searchLetter) === false)) {
+          alert("Invalid input!");
+        } else {
+          Game.guessedLetters.push(" " + searchLetter); //adds guessed letter to array of previous guesses//
+          var results = letterQuest(wordSplit, searchLetter);
+        } // end if input > 1 conditional
+      }else {
+        alert("Enter a letter jackass");
+      }
+
+
+      $("span#puzzle-array").text(results);
+      $("span#guessed-letters").text(Game.guessedLetters);
+      $("span#guesses-left").text("You have " + (6 + score) + " tries left");
+
+      //if you wanna watch...
+      // console.log(results);
+      console.log(wordSplit);
+      console.log(Game.guessedLetters);
+      console.log(score);
+
+      event.preventDefault();
+
+      if (score <= -6) {
+        alert("YOU LOSE, CHUMP");
+      }
 
     $("input#letter").val("");
+
   });
 });
